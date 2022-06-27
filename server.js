6d6c8cea0fbf41f178218c66f1c210569da26952
Khoa190601 <94828516+Khoa190601@users.noreply.github.com>
2022-06-27 20:04:49 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+
+const express = require('express')
+const connectionString ='mongodb+srv://anhkhoa:Abcd1234@cluster0.bgne9eq.mongodb.net/Store?retryWrites=true&w=majority'
+const bodyParser= require('body-parser')
+const app = express()
+
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+	.then(client => {
+		console.log('Connected to Database')
+    
+		const db = client.db('testdb')
+		const quotesCollection = db.collection('quotes')
+
+		// Make sure you place body-parser before your CRUD handlers!
+		app.use(bodyParser.urlencoded({ extended: true }))
+
+		app.set('view engine', 'ejs')
+
+		app.get('/', (req, res) => {
+			db.collection('quotes').find().toArray()
+			.then(results => {
+				res.render('index.ejs', { quotes: results })
+			})			
+			.catch(error => console.error(error))
+			
+			// ...
+			//res.sendFile(__dirname + '/index.html')
+		})
+
+		// get <form action="/quotes" method="POST"> from index.html after submission
+		app.post('/quotes', (req, res) => {
+		  quotesCollection.insertOne(req.body)
+			.then(result => {
+			  res.redirect('/')
+			})
+			.catch(error => console.error(error))
+		})
+
+
+		app.listen(process.env.PORT || 3000, function() {
+			console.log('listening on 3000')
+		})
+
+	
+	})
+	.catch(error => console.error(error))
